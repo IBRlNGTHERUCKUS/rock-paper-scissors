f43afde24722c1af9e97a0667d4a4af0fc05c983
Joseph C <josephalexcobian@gmail.com>
2023-09-06 19:05:50 -0700

    Add a game function to run the rounds a specified amount of time.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,10 +4,10 @@ function getUserChoice(){
 }
 
 function getBotChoice() {
-    let random = floor(Math.random() * 3);
+    let random = Math.floor((Math.random() * 3)) + 1;
     switch (random) {
         case 1: 
-            return "ROCK"
+            return "ROCK";
             break;
         case 2:
             return "PAPER";
@@ -18,23 +18,61 @@ function getBotChoice() {
     }
 }
 
-function rockPaperScissors() {
+// Returns winner of the round
+function playRound() {
     let userChoice = getUserChoice();
     let botChoice = getBotChoice();
-    let winner;
-    
+ 
     console.log(`User Choice: ${userChoice}`);
-    console.log(`User Choice: ${botChoice}`);
+    console.log(`Bot Choice: ${botChoice}`);
 
     if (userChoice == botChoice){
         console.log("Tie!");
+        return "tie";
     }
     else if (
         (userChoice == "ROCK" && botChoice === "SCISSORS") || 
         (userChoice == "PAPER" && botChoice === "ROCK") ||
         (userChoice == "SCISSORS" && botChoice === "PAPER")
-        ) {console.log("Congrats User!!!");}
-    else {console.log("You suck!!!")}
-
-
+        ) {
+            console.log("Congrats User!!!");
+            return "user";
+        }
+    else {
+        console.log("You suck!!!");
+        return "bot";
+    }
 }
+
+function game(rounds) {
+    let botScore = 0;
+    let userScore = 0;
+
+    // Run the game the specified rounds and keep score
+    for (let i = 0; i < rounds; i++) {
+        let winner = playRound();
+        if (winner == 'bot') {
+            botScore++;
+        }
+        else if (winner == 'user') {
+            userScore++;
+        }
+    }
+
+    //Determine a winner of the entire game
+    if (userScore > botScore) {
+        console.log("User wins game!");
+    }
+    else if (userScore < botScore) {
+        console.log("Bot wins game.");
+    }
+    else {
+        console.log("It's a tie game.");
+    }
+
+    //Display the score
+    console.log(`User Score: ${userScore}`)
+    console.log(`Bot Score: ${botScore}`)
+}
+
+game(7);
